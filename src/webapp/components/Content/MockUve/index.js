da95da03763508561d96d4507fe2ef2c1e8de93b
liuhao <liuhao@taou.com>
2020-11-18 08:43:52 +0800

    feat: 🐭 demo json

--- src/webapp/components/Content/MockUve/index.js
+++ src/webapp/components/Content/MockUve/index.js
@@ -21,7 +21,96 @@ class MockUve extends React.Component {
     super(props);
     this.state = {
       jsonStr: "", // TextArea 内容
-      json: "",
+      json: {
+        error_msg: "success",
+        return_code: 200,
+        id: "16049888380549988580",
+        respid: "16049888380549988580",
+        ext: {
+          timestamp_us: {
+            start: "1604988838163198",
+            end: "1604988838345651",
+          },
+          uve_timestamp: "1604988838162566",
+        },
+        faraday: {
+          big_banner_enable_profit_share_title: {
+            value: "true",
+          },
+          enable_lp_singelpage: {
+            value: "false",
+          },
+          enable_app_icon: {
+            value: "true",
+            description: "layer-2149#exp-9964",
+          },
+          is_big_card: {
+            value: "0",
+          },
+        },
+        data: [
+          {
+            adengine_version: "3.0",
+            return_code: 200,
+            result: [
+              {
+                mediainfo: [
+                  {
+                    is_filter_duplicated: 1,
+                    autoplay: 0,
+                  },
+                ],
+                cust_id: "6132666105",
+                structs: [
+                  {
+                    name: "",
+                    scheme: "",
+                  },
+                ],
+                object_id: "4570142530604425",
+                is_follow: false,
+                ad_tag: "2",
+                monitor_log: {
+                  adid: "10096150",
+                },
+                recommend: "广告",
+                dynamic_creative: [],
+                tag: "其他",
+                monitor_url: [
+                  {
+                    wb_real_expo:
+                      "https://vs.biz.weibo.com/wb_real_expo?p=783&s=01&m=45%5F1604988838054998858045000010096150&k=559bf2b61ceaf999c9d1bb1a4e516d2b&t=783%5F006",
+                  },
+                ],
+                render_type: 71,
+                optimization_objective: 86004001,
+                adtype: "1",
+                uid: "1843425801",
+                promotion_objective: 88030004,
+                is_dynamic_creative: false,
+                mark:
+                  "3_reallog_mark_ad:8|3_1576240979004515179605000006917291",
+              },
+            ],
+            service: "mainfeed",
+            business_type: "017830",
+            impression_id: "1604988838054998858045",
+            channel: "agent",
+            ext: {
+              freq_info: [
+                "1843425801_10096150_4559992101281391_70101011_6132666105_14_88030004_783_2730827_101_NULL_0_0_1_4559992101281391",
+              ],
+              bid_type: 1,
+              ad_settle_channel: "1",
+            },
+            titles: [],
+            position: 3,
+            render_flag: 1,
+            cand_type: "783_006",
+            ecpm: 200,
+          },
+        ],
+      },
       service: "main_feed",
       style: "see_download",
       port: "",
